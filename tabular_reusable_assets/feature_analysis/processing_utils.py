bd409f5def5e651334c98934d71d811b60c4eb4c
Kian Yew Ngieng <kianyewngieng@Kians-MacBook-Air.local>
2025-01-05 19:05:54 +0800

    add psi calculation for each feature

---
+++ tabular_reusable_assets/feature_analysis/processing_utils.py
@@ -0,0 +1,102 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+
+def get_equal_width_bins(data_feat: pd.Series, nbins=10) -> List:
+    min_value = min(data_feat)
+    max_value = max(data_feat)
+    delta = max_value - min_value
+    breaks = [min_value + 0.1 * i * delta for i in range(nbins + 1)]
+    return breaks
+
+
+def get_quantile_bins(data_feat: pd.Series, nbins=10) -> List:
+    quantiles = np.linspace(start=0, stop=1, num=nbins + 1)
+    breaks = [data_feat.quantile(q) for q in quantiles]
+    breaks[0] = breaks[0] * 0.99 if breaks[0] > 0 else breaks[0] * 1.01
+    # print(data_feat)
+    # print(breaks, quantiles)
+    return breaks
+
+
+def bin_feat(data, feat, breaks, include_lowest=False, precision=3, duplicates="drop", return_categorical=False):
+    if not return_categorical:
+        # return str
+        return pd.cut(
+            data[feat], bins=breaks, include_lowest=include_lowest, precision=precision, duplicates=duplicates
+        ).astype(str)
+    return pd.cut(data[feat], bins=breaks, include_lowest=include_lowest, precision=precision, duplicates=duplicates)
+
+
+def qcut(
+    data,
+    feat,
+    nbins,
+    include_lowest=False,
+    precision=3,
+    duplicates="drop",
+    return_categorical=False,
+    return_bins=False,
+):
+    breaks = get_quantile_bins(data[feat], nbins=nbins)
+
+    feat_binned = bin_feat(
+        data=data,
+        feat=feat,
+        breaks=breaks,
+        include_lowest=include_lowest,
+        precision=precision,
+        duplicates=duplicates,
+        return_categorical=return_categorical,
+    )
+    if not return_bins:
+        return feat_binned
+    else:
+        return feat_binned, breaks
+
+
+def equal_width_cut(
+    data,
+    feat,
+    nbins,
+    include_lowest=False,
+    precision=3,
+    duplicates="drop",
+    return_categorical=False,
+    return_bins=False,
+):
+    breaks = get_equal_width_bins(data[feat], nbins=nbins)
+    feat_binned = bin_feat(
+        data=data,
+        feat=feat,
+        breaks=breaks,
+        include_lowest=include_lowest,
+        precision=precision,
+        duplicates=duplicates,
+        return_categorical=return_categorical,
+    )
+
+    if not return_bins:
+        return feat_binned
+    else:
+        return feat_binned, breaks
+
+
+def calculate_feat_psi(left_df, right_df, feat, left_df_name="A", right_df_name="B", eps=np.finfo(float).eps):
+    A = left_df_name
+    B = right_df_name
+    left_vc = left_df[feat].value_counts(dropna=False, normalize=True).rename(A).reset_index()
+    right_vc = right_df[feat].value_counts(dropna=False, normalize=True).rename(B).reset_index()
+    vc = left_vc.merge(right_vc, on=[feat], how="outer").fillna(0)
+    vc["var"] = feat
+    vc = vc.sort_values(by=feat).rename(columns={feat: "bin"})
+
+    vc[f"{A}-{B}"] = vc[A] - vc[B]
+    vc[f"ln({A}/{B})"] = np.log(vc[A] / (vc[B] + eps))
+    vc["psi"] = vc[f"{A}-{B}"] * vc[f"ln({A}/{B})"]
+    vc["sum_psi"] = vc["psi"].sum()
+
+    columns = ["var", "bin"] + [col for col in vc.columns if col not in ["var", "bin"]]
+    return vc[columns]
--- tests/processing_utils/test_binning.py
+++ tests/processing_utils/test_binning.py
@@ -27,3 +27,35 @@ def test_quantile_bin(titanic_dataset):
     # additional for start
     assert len(quantile_bins) == 4 + 1
     assert np.allclose(vc1.values, vc2.values)
+
+
+def test_psi_calculation(titanic_dataset):
+    data = titanic_dataset["data"]
+    # feature_columns = titanic_dataset["feature_columns"]
+    # target_column = titanic_dataset["target_column"]
+
+    data["qAge"] = processing_utils.qcut(data, "Age", nbins=10)
+    data2 = data.sample(frac=0.5, random_state=99)
+
+    indiv_psi_table = processing_utils.calculate_feat_psi(left_df=data, right_df=data2, feat="qAge")
+    assert list(indiv_psi_table.columns) == ["var", "bin", "A", "B", "A-B", "ln(A/B)", "psi", "sum_psi"]
+
+    # check element wise operation
+    row = indiv_psi_table.iloc[0]
+    assert (row["A-B"] * row["ln(A/B)"]) == row["psi"]
+
+
+def test_psi_detect_different_distribution(titanic_dataset):
+    data = titanic_dataset["data"]
+
+    data["qAge"] = processing_utils.qcut(data, "Age", nbins=10)
+
+    # very small sample, psi should be high
+    data2 = data.sample(1, random_state=99)
+    indiv_psi_table = processing_utils.calculate_feat_psi(left_df=data, right_df=data2, feat="qAge")
+    assert indiv_psi_table["psi"].unique()[0] > 0.2
+
+    # same sample, psi should be small close to 0
+    data2 = data.sample(frac=1, random_state=99)
+    indiv_psi_table = processing_utils.calculate_feat_psi(left_df=data, right_df=data2, feat="qAge")
+    assert indiv_psi_table["psi"].unique()[0] == 0
